7d32c431798ff920e5ae6f2abad01f9bc5e5dbd6
yodenharsh <harshmorayya@ymail.com>
2022-10-31 09:37:01 +0530

    Added index.php

--- someProject/index.php
+++ someProject/index.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <link rel="stylesheet" type="text/css" href="/someProject/styles.css">
+    <title>Placeholder title</title>
 </head>
 
 <body>
@@ -14,14 +15,14 @@
     </div>
     <ul class="main-list">
         <li>
-            <div>
+            <div class="student-login-card">
                 <h3>Student login</h3>
-                <form>
-                    <div>
+                <form action="SOT\student_exam_details.php">
+                    <div class="list-item">
                         <label for="rollno">Roll No./UGID </label>
                         <input type="text" name="rollno" title="rollno" />
                     </div>
-                    <div>
+                    <div class="list-item">
                         <label for="school">Select school </label>
                         <select name="school">
                             <option value="sot">School of Technology </option>
@@ -31,17 +32,24 @@
                             <option value="soah">School of Arts & Humanities</option>
                         </select>
                     </div>
+                    <div class="list-item">
+                        <button type="submit" class="st-btn btn">Login and View Marksheet</button>
+                    </div>
                 </form>
-                <a href="SOT\student_exam_details.php"><button>Login and View Marksheet</button></a>
             </div>
         </li>
         <li>
-            <div>
+            <div class="coe-login-card">
                 <h3>COE login</h3>
-                <div>
-                    <label for="password">Enter password: </label>
-                    <input type="password" name="password" title="password" />
-                </div>
+                <form>
+                    <div>
+                        <label for="password">Enter password: </label>
+                        <input type="password" name="password" title="password" />
+                    </div>
+                    <div class="list-item">
+                        <a href="#"><button class="coe-btn btn">Placeholder button</button></a>
+                    </div>
+                </form>
             </div>
         </li>
     </ul>
